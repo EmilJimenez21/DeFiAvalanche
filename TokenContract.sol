d3e747bd2d82fef97647f1971cba173f145c0b45
Emil Jimenez <1885357+EmilJimenez21@users.noreply.github.com>
2022-03-03 13:42:57 -0500

    Update and rename FireToken.sol to TokenContract.sol

--- TokenContract.sol
+++ TokenContract.sol
@@ -4,8 +4,8 @@ pragma solidity 0.8.11;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract FireToken is ERC20, Ownable {
-    constructor(address owner, uint256 tokenCount) ERC20("FIRE", "FIRE") {
+contract TokenContract is ERC20, Ownable {
+    constructor(address owner, uint256 tokenCount) ERC20("Token", "TOKEN") {
         super._mint(owner, tokenCount * 10**18);
     }
 }
